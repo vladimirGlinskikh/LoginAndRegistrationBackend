7833467ec4c61dffd51666c6947ad15e68e68f29
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-12-13 20:31:33 +0600

    Registration Service

--- src/main/java/kz/zhelezyaka/loginandregistrationbackend/appuser/AppUserService.java
+++ src/main/java/kz/zhelezyaka/loginandregistrationbackend/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_MSG =
             "User with email %s not found";
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String email)
@@ -19,5 +21,20 @@ public class AppUserService implements UserDetailsService {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(() ->
                         new UsernameNotFoundException(String.format(USER_NOT_FOUND_MSG, email)));
+    }
+
+    public String signUpUser(AppUser appUser) {
+        final boolean userExists = appUserRepository
+                .findByEmail(appUser.getEmail())
+                .isPresent();
+        if (userExists) {
+            throw new IllegalStateException("email already taken");
+        }
+        final String encodedPassword = bCryptPasswordEncoder
+                .encode(appUser.getPassword());
+        appUser.setPassword(encodedPassword);
+        appUserRepository.save(appUser);
+        // TODO: Send confirmation token
+        return "It works";
     }
 }
--- src/main/java/kz/zhelezyaka/loginandregistrationbackend/registration/RegistrationService.java
+++ src/main/java/kz/zhelezyaka/loginandregistrationbackend/registration/RegistrationService.java
@@ -1,10 +1,31 @@
 package kz.zhelezyaka.loginandregistrationbackend.registration;
 
+import kz.zhelezyaka.loginandregistrationbackend.appuser.AppUser;
+import kz.zhelezyaka.loginandregistrationbackend.appuser.AppUserRole;
+import kz.zhelezyaka.loginandregistrationbackend.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "It Works";
+        final boolean isValidEmail =
+                emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
